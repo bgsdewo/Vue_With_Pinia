62e49d5426dd105b5a771b397442e22986c89f5e
unknown <bgsdewoo20@gmail.com>
2024-06-16 14:43:40 +0700

    Memisahkan State Dengan Composable

--- vue-state-management/src/components/GrandChild.vue
+++ vue-state-management/src/components/GrandChild.vue
@@ -1,9 +1,14 @@
 <script setup>
 import GreatGrandChild from './GreatGrandChild.vue'
+import useNumbers from '../composable/useNumbers'
+
+
+const { numbers } = useNumbers()
 </script>
 <template>
     <div>
         <h1>GrandChild</h1>
+        {{ numbers }}
         <div class="line"></div>
         <GreatGrandChild />
     </div>
--- vue-state-management/src/components/Parent.vue
+++ vue-state-management/src/components/Parent.vue
@@ -1,10 +1,15 @@
 <script setup>
 import Child from './Child.vue'
+import useNumbers from '../composable/useNumbers'
+
+const { numbers,addNumber } = useNumbers()
 </script>
 
 <template>
     <div>
         <h1>Parent</h1>
+        {{ numbers }}
+        <button @click="addNumber">Add</button>
         <div class="line"></div>
         <Child />
     </div>
